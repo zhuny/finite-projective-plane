02f2a00a36cadfd8443f47f6911767025b0143c6
zhuny <zhuny@kaist.ac.kr>
2021-11-30 22:42:43 +0900

    check element size

--- plane/__main__.py
+++ plane/__main__.py
@@ -11,20 +11,27 @@ def run():
         return
 
     with Path(sys.argv[1]).open(encoding='utf8') as f:
+        # Get degree of finite projective plane
         n = f.readline().strip()
         if not n.isdigit():
             print("First line should be a prime number.")
             return
 
         n = int(n)
-        elements = set()
 
+        # load element
+        elements = set()
         for line in f:
             elements.update(line.split())
 
+        if len(elements) < n*n + n + 1:
+            print("The number of elements should be "
+                  "greater than or equal to 'n*n+n+1'")
+            return
+
         structure = construct(
             FiniteField(n),
-            elements
+            list(elements)
         )
         for g in structure:
             print(",".join(g))
