d491493e3071818b8615319d25cbdd86b0650c25
zhuny <zhuny@kaist.ac.kr>
2021-11-30 21:48:55 +0900

    get orthogonal vector relation

--- plane/__main__.py
+++ plane/__main__.py
@@ -3,7 +3,10 @@ from plane.projective import construct
 
 
 def run():
-    construct(FiniteField(7))
+    construct(
+        FiniteField(7),
+        list(range(57))
+    )
 
 
 if __name__ == '__main__':
--- plane/projective.py
+++ plane/projective.py
@@ -1,12 +1,22 @@
+import random
+
 from plane.number import Field
 from plane.vector import ProjectivePlane
 
 
-def construct(field: Field):
+def construct(field: Field, elements):
     plane = ProjectivePlane(field)
     line_list = list(plane.get_point_list())
+    random.shuffle(line_list)
+    pair_list = list(zip(line_list, elements))
 
-    for line in line_list:
-        for normal in line_list:
-            if line.is_orthogonal(normal):
-                print(line, normal)
+    assert len(line_list) == len(pair_list)
+
+    for normal in line_list:
+        plane_element = [
+            element
+            for line, element in pair_list
+            if line.is_orthogonal(normal)
+        ]
+        print(plane_element)
+    print(len(line_list))
