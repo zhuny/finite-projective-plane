84e150346c6844a28b97e3bc8b5eb133d379d05e
zhuny <zhuny@kaist.ac.kr>
2021-11-30 22:38:03 +0900

    change to command

--- plane/__main__.py
+++ plane/__main__.py
@@ -1,12 +1,33 @@
+import sys
+from pathlib import Path
+
 from plane.number import FiniteField
 from plane.projective import construct
 
 
 def run():
-    construct(
-        FiniteField(7),
-        list(range(57))
-    )
+    if len(sys.argv) <= 1:
+        print(sys.argv[0], "{info.txt}")
+        return
+
+    with Path(sys.argv[1]).open(encoding='utf8') as f:
+        n = f.readline().strip()
+        if not n.isdigit():
+            print("First line should be a prime number.")
+            return
+
+        n = int(n)
+        elements = set()
+
+        for line in f:
+            elements.update(line.split())
+
+        structure = construct(
+            FiniteField(n),
+            elements
+        )
+        for g in structure:
+            print(",".join(g))
 
 
 if __name__ == '__main__':
