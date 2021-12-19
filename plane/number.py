0e61205aac694a86f815595ebf39a2336c2a4be7
noah-zhuny <noah@axchange.co>
2021-12-19 14:12:37 +0900

    polynomial

--- plane/number.py
+++ plane/number.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
-from typing import List, Union
+from dataclasses import dataclass, field as data_field
+from typing import Union, Dict
+
+from plane.util import factorize
 
 
 @dataclass
@@ -28,6 +30,12 @@ class FiniteFieldElement:
     def __post_init__(self):
         self.number %= self.finite_field.order
 
+    def __sub__(self, other: 'FiniteFieldElement'):
+        return FiniteFieldElement(
+            self.number - other.number,
+            self.finite_field
+        )
+
     def __mul__(self, other: 'FiniteFieldElement'):
         return FiniteFieldElement(
             self.number * other.number,
@@ -40,21 +48,192 @@ class FiniteFieldElement:
             self.finite_field
         )
 
+    def __neg__(self):
+        return FiniteFieldElement(-self.number, self.finite_field)
+
+    def __truediv__(self, other):
+        return FiniteFieldElement(
+            self.number * pow(other.number, -1, self.finite_field.order),
+            self.finite_field
+        )
+
     def is_zero(self):
         return self.number == 0
+
+
+class AddContainer:
+    def __init__(self):
+        self.body = {}
+
+    def update(self, k, v):
+        if k in self.body:
+            self.body[k] += v
+        else:
+            self.body[k] = v
+
+
+@dataclass
+class Polynomial:
+    over: FiniteField
+    values: Dict[int, FiniteFieldElement] = data_field(default_factory=dict)
+    degree: int = data_field(init=False)
+
+    def __post_init__(self):
+        self.values = {
+            p: v for p, v in self.values.items()
+            if not v.is_zero()
+        }
+        if self.values:
+            self.degree = max(self.values)
+        else:
+            # deg(f*g) = deg(f) + deg(g) 를 맞추기 위해 0의 경우,
+            # degree를 negative infinite로 두지만 지금은 그럴 필욘 없다.
+            self.degree = -1
+
+    @property
+    def lead_coefficient(self) -> FiniteFieldElement:
+        return self.values[self.degree]
+
+    def is_zero(self):
+        return self.degree == -1
+
+    def __add__(self, other: 'Polynomial'):
+        d = dict(self.values)
+        for k, v in other.values.items():
+            if k in d:
+                d[k] += v
+            else:
+                d[k] = v
+        return Polynomial(over=self.over, values=d)
+
+    def __sub__(self, other: 'Polynomial'):
+        d = dict(self.values)
+        for k, v in other.values.items():
+            if k in d:
+                d[k] -= v
+            else:
+                d[k] = -v
+        return Polynomial(over=self.over, values=d)
+
+    def __mul__(self, other: Union[FiniteFieldElement, 'Polynomial']):
+        if isinstance(other, FiniteFieldElement):
+            return Polynomial(
+                over=self.over,
+                values={
+                    p: v*other
+                    for p, v in self.values.items()
+                }
+            )
+
+        elif isinstance(other, Polynomial):
+            result = AddContainer()
+            for k1, v1 in self.values.items():
+                for k2, v2 in other.values.items():
+                    result.update(k1+k2, v1*v2)
+            return Polynomial(over=self.over, values=result.body)
+
+    def __mod__(self, other: 'Polynomial'):
+        divisor = self
+        q = Polynomial(over=self.over, values={})
+
+        while divisor.degree >= other.degree:
+            degree_diff = divisor.degree - other.degree
+            div_coefficient = divisor.lead_coefficient / other.lead_coefficient
+            div = Polynomial(
+                over=self.over,
+                values={
+                    degree_diff: div_coefficient
+                }
+            )
+            q += div
+            divisor -= other * div
+
+        return divisor
+
+    def gcd(self, other: 'Polynomial'):
+        first, second = self, other
+        if first.degree < second.degree:
+            first, second = second, first
+
+        while not second.is_constant():
+            print("gcd-step", first.get_min(), second.get_min())
+            first, second = second, first % second
+
+        print("gcd-step", first.get_min(), second.get_min())
+
+        return first
+
+    def is_constant(self):
+        return self.degree == 0
+
+    def is_irreducible(self) -> bool:
+        # https://en.wikipedia.org/wiki/Factorization_of_polynomials_over_finite_fields#Rabin's_test_of_irreducibility
+        if len(self.values) == 0:
+            return False
+
+        degree = max(self.values)
+        for p in factorize(degree):
+            test_polynomial = Polynomial(
+                over=self.over,
+                values={
+                    self.over.order ** (degree // p): self.over.one,
+                    1: -self.over.one
+                }
+            )
+            print("two poly", test_polynomial.get_min(), self.get_min())
+            print("gcd", test_polynomial.gcd(self).get_min())
+            if not test_polynomial.gcd(self).is_constant():
+                return False
+
+        return True
+
+    def get_min(self):
+        return {k: v.number for k, v in self.values.items()}
 
 
 @dataclass
 class ExtensionFiniteField:
     finite_field: FiniteField
     degree: int  # >= 2
+    mod: Polynomial
 
 
 @dataclass
 class ExtensionFiniteFieldElement:
     # Element of Extension Finite Field
     extension_finite_field: ExtensionFiniteField
-    numbers: List[FiniteFieldElement]
+    numbers: Polynomial
 
 
 Field = Union[FiniteField, ExtensionFiniteField]  # 사칙연산이 가능한 수 쳬계
+
+
+def find_irreducible(field: FiniteField, degree: int) -> Polynomial:
+    if degree < 2:
+        raise ValueError("Degree should be greater than 1")
+
+    for first in field.element_list():
+        for second in field.element_list():
+            polynomial = Polynomial(
+                over=field,
+                values={
+                    degree: field.one,
+                    1: first, 0: second
+                }
+            )
+            if polynomial.is_irreducible():
+                return polynomial
+
+
+def get_field(n: int) -> Field:
+    factor = factorize(n)
+    if len(factor) != 1:
+        raise ValueError("Field element should be the power of prime number")
+
+    for number, p in factor.items():
+        base_field = FiniteField(number)
+        if p == 1:
+            return base_field
+        else:
+            return ExtensionFiniteField(base_field, p, find_irreducible(base_field, p))
+    return FiniteField(n)
--- plane/util.py
+++ plane/util.py
@@ -1,4 +1,7 @@
-def is_prime(n):
+from typing import Dict
+
+
+def is_prime(n: int) -> bool:
     if not isinstance(n, int):
         # raise ValueError("Integer should be given.")
         return False
@@ -19,3 +22,22 @@ def is_prime(n):
             break
 
     return True
+
+
+def factorize(n: int) -> Dict[int, int]:
+    factor = {}
+    for i in range(2, n):  # range 가 리스트를 미리 생성해 두지 않아 complexity 에 영향이 없음
+        if n < i*i:
+            break
+
+        p = 0
+        while n % i == 0:
+            p += 1
+            n //= i
+        if p > 0:
+            factor[i] = p
+
+    if n > 1:
+        factor[n] = 1
+
+    return factor
